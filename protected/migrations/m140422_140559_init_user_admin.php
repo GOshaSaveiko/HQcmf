60880e2a3909e64cc64920ebf1a9a8f024558c12
Andrey Azarko <andrey@iwl.by>
2014-04-22 14:26:18 +0300

    add migration init_user_admin

---
+++ protected/migrations/m140422_140559_init_user_admin.php
@@ -0,0 +1,30 @@
+<?php
+
+class m140422_140559_init_user_admin extends CDbMigration
+{
+	public function up()
+	{
+        $this->insert('user',array(
+           'u_login'=>'admin',
+           'u_pass'=>'admin',
+           'u_mail'=>'admin@admin.com',
+        ));
+	}
+
+	public function down()
+	{
+		$this->delete('user','u_login=:u_login',array(':u_login'=>'admin'));
+		return true;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
